647e804bc45b6b74f6cd1e65a7d774ef2b6f76e8
kasper084 <kasper084@gmail.com>
2020-02-01 13:55:11 +0200

    dev branch

--- src/main/java/filter/App.java
+++ src/main/java/filter/App.java
@@ -1,13 +1,8 @@
 package filter;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
     }
 }
